ba0f20d4777266c68a21ae08dd9c8b4cd6040755
ismail_akhadov11 <ismail.tolosh@gmail.com>
2020-09-01 00:55:46 +0300

    urls refactored

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from main.views import PostListView, PostCreateView, UserPostsView
 
 urlpatterns = [
-    path("posts/", PostListView.as_view(), name="posts-list"),
-    path("posts/create/", PostCreateView.as_view(), name="post-create"),
-    path("posts/user_posts/", UserPostsView.as_view(), name="user-posts"),
+    path("", PostListView.as_view(), name="posts-list"),
+    path("create/", PostCreateView.as_view(), name="post-create"),
+    path("user_posts/", UserPostsView.as_view(), name="user-posts"),
 ]
