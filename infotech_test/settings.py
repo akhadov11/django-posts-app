97082614648d8a27673ac1c6fd4f7e8db3813dce
ismail_akhadov11 <ismail.tolosh@gmail.com>
2020-09-01 01:58:53 +0300

    quick fix

--- infotech_test/settings.py
+++ infotech_test/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "a!#0qx^8ij-uk=-5(+24=z3w$*sb$r*+)^ygsqg$@-(co4$rbg")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
